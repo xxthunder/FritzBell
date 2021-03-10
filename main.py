cc53694e85d03d7a0e41b5627080cbabc7d6cf92
Karsten A. M. Guenther <karsten.guenther@kamg.de>
2021-03-10 08:09:40 +0100

    Python script for internal phone call

---
+++ main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+import argparse
+import time
+
+from fritzconnection import FritzConnection
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+class Caller:
+    def __init__(self, hostname, port, user, passwd):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.name = 'Caller'
+        self.hostname = hostname
+        self.port = port
+        self.user = user
+        self.passwd = passwd
+
+    def run(self, number, timeout=0):
+        self.logger.info('Calling ' + number)
+
+        my_fritz_connection = FritzConnection(self.hostname, self.port, self.user, self.passwd)
+        self.logger.info(my_fritz_connection.modelname)
+
+        # Todo: what about service discovery first?
+        my_fritz_connection.call_action('X_VoIP1', 'X_AVM-DE_DialNumber', arguments={'NewX_AVM-DE_PhoneNumber': number})
+
+        # Todo: maybe this should be a separate function?
+        if timeout > 0:
+            time.sleep(timeout)
+            my_fritz_connection.call_action('X_VoIP1', 'X_AVM-DE_DialHangup')
+
+        return True
+
+
+def main():
+    argument_parser = argparse.ArgumentParser(description='FritzBell')
+    argument_parser.add_argument('--hostname', required=False, help='Hostname of your Fritz!Box (default: fritz.box)')
+    argument_parser.add_argument('--port', required=False, help='TR064 port of your Fritz!Box (default: 49000)')
+    argument_parser.add_argument('--user', required=True, help='Fritz!Box user name')
+    argument_parser.add_argument('--passwd', required=True, help='Fritz!Box user\'s password')
+    argument_parser.add_argument(
+        '--number',
+        required=True,
+        help='Number to be called, e.g., \'**9\' for broadcast call (every connected phone).'
+    )
+    args = argument_parser.parse_args()
+
+    # Handle optional arguments
+    hostname = 'fritz.box'
+    if args.hostname:
+        hostname = args.hostname
+    port = 49000
+    if args.port:
+        port = args.port
+    my_caller = Caller(hostname, port, args.user, args.passwd)
+    my_caller.run(args.number)
+
+
+if __name__ == '__main__':
+    main()
